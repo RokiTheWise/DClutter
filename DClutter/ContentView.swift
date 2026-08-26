96378e592b751bdb682b210d2541468f061850d3
DJ <dexterjethro.enriquez@gmail.com>
2026-08-26 12:05:48 +0800

    Add Xcode app target with sandbox entitlement

---
+++ DClutter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DClutter
+//
+//  Created by Dexter Jethro Enriquez on 8/26/26.
+//
+
+import SwiftUI
+import DClutterCore
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DClutter/DClutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DClutterApp.swift
+//  DClutter
+//
+//  Created by Dexter Jethro Enriquez on 8/26/26.
+//
+
+import SwiftUI
+
+@main
+struct DClutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
